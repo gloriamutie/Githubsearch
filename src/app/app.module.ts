7704e088b9a7f1d8546cdda2f107a132f8aca7da
Gloria Mutie <mwongeliglo15@gmail.com>
2020-08-30 16:07:41 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { NavComponent } from './nav/nav.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { UserRepoListComponent } from './user-repo-list/user-repo-list.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DatepipePipe } from './datepipe.pipe';
+import { LogoHighlightDirective } from './logo-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserDataComponent,
+    NavComponent,
+    SearchUserComponent,
+    UserRepoListComponent,
+    NotFoundComponent,
+    DatepipePipe,
+    LogoHighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
